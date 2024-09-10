cca952652dc4da93cb5a69031e18a5dd1f52858e
jaykarlapalem <bhaskar.karlapalem@gmail.com>
2024-09-09 20:22:03 -0700

    Updated comments in Location manager

--- WeatherApp/Service/Response/LocationManager.swift
+++ WeatherApp/Service/Response/LocationManager.swift
@@ -22,18 +22,21 @@ class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
-
+    // CLLocationManagerDelegate method called when new location data is available
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
+        
+        // Reverse geocode the location to get the city name
         geocoder.reverseGeocodeLocation(location) { [weak self] placemarks, error in
             if let placemark = placemarks?.first {
                 DispatchQueue.main.async {
+                    // Update the city name on the main thread
                     self?.cityName = placemark.locality ?? "Unknown"
                 }
             }
         }
     }
-
+    // CLLocationManagerDelegate method called when location updates fail
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to find user's location: \(error.localizedDescription)")
     }
